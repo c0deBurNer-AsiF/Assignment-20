1abb79c74f3de78aafdacef0c141b50bcc956fd8
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-01-02 00:11:12 +0530

    Update qn-2.c

--- qn-2.c
+++ qn-2.c
@@ -29,3 +29,34 @@ void swap(char *p, char *q)
 
     printf("%s",q);
 }
+
+
+//printf in main function---
+
+
+/*#include<stdio.h>
+void swap(char *, char *);
+int main()
+{
+    char a[100],b[100];
+    swap(a,b);
+    printf("%s\n",a);
+    printf("%s",b);
+
+}
+void swap(char *p, char *q)
+{
+    char m[100],*t;
+    t=m;
+    int i;
+    printf("enter two names or words:\n");
+    gets(p);
+    gets(q);
+
+    for(i=0;*(p+i) || *(q+i); i++)
+    {
+        *(t+i)=*(p+i);
+        *(p+i)=*(q+i);
+        *(q+i)=*(t+i);
+    }
+}*/
