9c6e0c7639b5ecc73d1bd4a64cd22925c792578b
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-01-01 23:58:17 +0530

    Create qn-2.c

---
+++ qn-2.c
@@ -0,0 +1,31 @@
+//2. Write a function to swap strings of two char arrays of calling functions.
+
+
+#include<stdio.h>
+void swap(char *, char *);
+int main()
+{
+    char a[100],b[100];
+    swap(a,b);
+
+}
+void swap(char *p, char *q)
+{
+    char m[100],*t;
+    t=m;
+    int i;
+    printf("enter two names or words:\n");
+    gets(p);
+    gets(q);
+
+    for(i=0;*(p+i) || *(q+i); i++)
+    {
+        *(t+i)=*(p+i);
+        *(p+i)=*(q+i);
+        *(q+i)=*(t+i);
+    }
+
+    printf("%s\n",p);
+
+    printf("%s",q);
+}
