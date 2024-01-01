1731f8250f279df1c88df5606529a5851e17fea2
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-01-01 20:48:07 +0530

    Create qn-8.c

---
+++ qn-8.c
@@ -0,0 +1,25 @@
+//8. Write a program to compute the sum of all elements in an array using pointers.
+
+
+#include<stdio.h>
+int main()
+{
+    int a[100],i,x,sum=0;
+    printf("enter the size of an array:\n");
+    scanf("%d",&x);
+    printf("enter %d numbers:\n",x);
+
+    for(i=0;i<x;i++)
+    scanf("%d",&a[i]);
+
+    int *p;
+    p=a;
+
+    for(i=0;i<x;i++)
+    {
+        sum=sum+*(p+i);
+    }
+    printf("sum of the array is %d",sum);
+
+
+}
