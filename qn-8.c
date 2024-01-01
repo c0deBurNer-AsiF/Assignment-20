8fc45dab06ee2f85508527b57b84d2780e2eda2f
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-01-01 21:04:45 +0530

    Update qn-8.c

--- qn-8.c
+++ qn-8.c
@@ -4,16 +4,16 @@
 #include<stdio.h>
 int main()
 {
-    int a[100],i,x,sum=0;
+    int a[100],i,x,sum=0,*p;
     printf("enter the size of an array:\n");
     scanf("%d",&x);
+    p=a;
     printf("enter %d numbers:\n",x);
 
-    for(i=0;i<x;i++)
-    scanf("%d",&a[i]);
 
-    int *p;
-    p=a;
+    for(i=0;i<x;i++)
+    scanf("%d",(p+i));
+
 
     for(i=0;i<x;i++)
     {
